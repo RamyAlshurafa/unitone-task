188940eb7e21549f720b167c4b653981f540edd4
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-26 21:59:26 +0200

    add provider into the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import EmailsStore from './stores/EmailsStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = (
+
+  <Provider EmailsStore={EmailsStore}>
+    <App />
+  </Provider>
+)
+ReactDOM.render(Root, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
