610a9821e21468001a8b69fbdb99267a21fd3b43
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-28 00:37:07 +0200

    update emails store

--- client/src/stores/EmailsStore.js
+++ client/src/stores/EmailsStore.js
@@ -1,7 +1,35 @@
-import { observable } from "mobx";
+import { observable, action, computed } from 'mobx';
+import axios from 'axios';
 
 class EmailsStore {
-  @observable Emails = [];
+  @observable emails = [];
+
+  @observable activeEmailIndex;
+
+  @observable state = 'loading' // "loading" / "done" / "error"
+
+  @action
+  changeActiveIndex = (index) => {
+    this.activeEmailIndex = index;
+  }
+
+  @computed get newEmailsCount() {
+    return this.emails.filter(item => item.new).length;
+  }
+
+  @computed get totalEmailsCount() {
+    return this.emails.length;
+  }
+
+  @action
+  fetchEmails = () => {
+    this.emails = [];
+    this.state = 'loading';
+    axios.get('/emails').then(({ data: { data } }) => {
+      this.state = 'done';
+      this.emails = data;
+    });
+  }
 }
 
 export default new EmailsStore();
